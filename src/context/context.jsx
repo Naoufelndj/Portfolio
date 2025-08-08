be7467553c21f2c17e36870a7634bb45a4bfbbaf
Naoufel ndj <naoufelnedjaoum@gmail.com>
2025-08-08 03:03:51 +0100

    deleted focus point

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -39,7 +39,6 @@ import Atélia from "../assets/images/projects/Thumbnail/Atélia.webp";
 import Opulence from "../assets/images/projects/Thumbnail/Opulence.webp";
 import Brewora from "../assets/images/projects/Thumbnail/Brewora.webp";
 import ReveBlanc from "../assets/images/projects/Thumbnail/ReveBlanc.webp";
-import FocusPoint from "../assets/images/projects/Thumbnail/FocusPoint.webP";
 
 import FlavorBiteModal from "../assets/images/projects/Modal/FlavorBiteFull.webp";
 import PalmoraModal from "../assets/images/projects/Modal/PalmoraFull.webp";
@@ -80,7 +79,6 @@ import AtéliaModal from "../assets/images/projects/Modal/AtéliaFull.webp";
 import OpulenceModal from "../assets/images/projects/Modal/OpulenceFull.webp";
 import BreworaModal from "../assets/images/projects/Modal/BreworaFull.webp";
 import ReveBlancModal from "../assets/images/projects/Modal/ReveBlancFull.webp";
-import FocusPointModal from "../assets/images/projects/Modal/FocusPointFull.webP";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const appContext = createContext();
@@ -157,15 +155,6 @@ const projects = [
       "Photography portfolio website with clean gallery-style layout",
     modal: LunoraStudioModal,
   },
-  {
-    id: 10,
-    image: FocusPoint,
-    title: "Focus Point - Photography Studio",
-    description:
-      "Photography studio website with bold visuals and modern aesthetic",
-    modal: FocusPointModal,
-  },
-
   {
     id: 11,
     image: TavolaFresca,
