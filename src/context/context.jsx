1c6d01bff523c3593c3e2f6f17e342a0305c8169
Naoufel ndj <naoufelnedjaoum@gmail.com>
2025-08-08 02:07:17 +0100

    Added focus point project

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -38,8 +38,8 @@ import Solvera from "../assets/images/projects/Thumbnail/Solvera.webp";
 import Atélia from "../assets/images/projects/Thumbnail/Atélia.webp";
 import Opulence from "../assets/images/projects/Thumbnail/Opulence.webp";
 import Brewora from "../assets/images/projects/Thumbnail/Brewora.webp";
-import FocusPoint from "../assets/images/projects/Thumbnail/FocusPoint.webp";
 import ReveBlanc from "../assets/images/projects/Thumbnail/ReveBlanc.webp";
+import FocusPoint from "../assets/images/projects/Thumbnail/FocusPoint.webp";
 
 import FlavorBiteModal from "../assets/images/projects/Modal/FlavorBiteFull.webp";
 import PalmoraModal from "../assets/images/projects/Modal/PalmoraFull.webp";
@@ -79,8 +79,8 @@ import SolveraModal from "../assets/images/projects/Modal/SolveraFull.webp";
 import AtéliaModal from "../assets/images/projects/Modal/AtéliaFull.webp";
 import OpulenceModal from "../assets/images/projects/Modal/OpulenceFull.webp";
 import BreworaModal from "../assets/images/projects/Modal/BreworaFull.webp";
-import FocusPointModal from "../assets/images/projects/Modal/FocusPointFull.webp";
 import ReveBlancModal from "../assets/images/projects/Modal/ReveBlancFull.webp";
+import FocusPointModal from "../assets/images/projects/Modal/FocusPointFull.webp";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const appContext = createContext();
